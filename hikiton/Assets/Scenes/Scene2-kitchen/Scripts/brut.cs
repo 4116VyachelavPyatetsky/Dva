2c189cba150fe75c1255bace7e5f14cb568b6212
Snake mage <115720162+4116VyachelavPyatetsky@users.noreply.github.com>
2024-10-27 17:09:39 +0300

    new spr

--- hikiton/Assets/Scenes/Scene2-kitchen/Scripts/brut.cs
+++ hikiton/Assets/Scenes/Scene2-kitchen/Scripts/brut.cs
@@ -4,9 +4,17 @@ using UnityEngine;
 
 public class brut : MonoBehaviour
 {
+    public bool once = true;
+    public static bool made = false;
     // Start is called before the first frame update
     void Start()
     {
-        transform.GetComponent<Animator>().SetTrigger("Pereh");
+        if (once && !made)
+        {
+            transform.GetComponent<Animator>().SetTrigger("Pereh");
+            made = true;
+        }
+        else { 
+            transform.GetComponent<Animator>().SetTrigger("Pereh"); }
     }
 }
